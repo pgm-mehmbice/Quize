50f9877e645e8eb6e0525b4a95cd85b506fb2522
pgm-mehmbice <mehmbice@student.arteveldehs.be>
2021-03-09 17:43:06 +0100

    Start

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const app = {
+    init() {
+      this.cacheElements();
+    },
+    cacheElements() {
+    //   this.$ = document.querySelector("");
+    //   this.$ = document.querySelector("");
+    },
+
+    
+};
+app.init();
