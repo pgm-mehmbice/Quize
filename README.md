f3d5da68284de3fcabad2dfbf81a6d8136be7e7b
pgm-mehmbice <mehmbice@student.arteveldehs.be>
2021-03-10 15:33:04 +0100

    Geprobeert te maken

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Quize
 Opdracht Quize
+
+
+|           |                                |
+| --------- | ------------------------------ |
+| Firstname | Mehmet                         |
+| Lastname  | Bicer                          |
+| Gender    | :male_sign:                    |
+| Email     | mehmbice@student.arteveldehs.be|
+
+**GRADUAAT PROGRAMMEREN ARTEVELDEHOGESCHOOL**
+We :heart: **Programming** :thumbsup: **Graphics** make **Cool** :poop:
--- js/app.js
+++ js/app.js
@@ -1,12 +1,30 @@
+import { JSON_PATH } from './catagories.js';
+
+
+const API_KEY = "JvOM1m9vfZTzdOgG5nd5PRXP5JNp9ipvYehq4wuO";
+
 const app = {
     init() {
       this.cacheElements();
+      this.fetchQuizAPI();
+      this.registerListeners();
     },
     cacheElements() {
-    //   this.$ = document.querySelector("");
+      this.$catagory = document.querySelector("#catagory");
+      this.$vragen = document.querySelector("#vragen");
+      this.$antwoorden = document.querySelector("#antwoorden");
     //   this.$ = document.querySelector("");
     },
 
     
+    async fetchQuizAPI() {
+      await fetch(`${QUIZZAPI}?apiKey=${QUIZZ_TOKEN}&catagory=${this.$quizCatagory.value}&difficulty=${$quizDifficulty.value}&limit=${this.quizAmount.value}&tags=${this.quizTags.value}`)
+      .than(response => response.json())
+      .then(json => {
+        tempr += ``
+
+      })
+    }
 };
 app.init();
+
